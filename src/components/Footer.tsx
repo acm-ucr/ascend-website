0dc73b317b1b6121eb9fdc847182bef76641716e
kshuang04 <shuan284@ucr.edu>
2025-10-11 22:29:40 -0700

    Added changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,31 +7,41 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="grid h-1/3 w-screen grid-cols-3 items-center p-8">
-      <div className="ml-8 flex w-5/8">
-        <Image src={ascendLogo} alt="Ascend Logo" />
-      </div>
-      <div></div>
+    <div className="flex h-1/3 w-screen items-center justify-between px-32 py-8">
+      <Image src={ascendLogo} className="w-1/4" alt="Ascend Logo" />
+
       <div className="flex flex-col text-left">
-        <div
-          className="text-4xl font-bold"
-          style={{ fontFamily: "var(--font-playfair)" }}
+        <div className="font-playfair text-4xl font-bold">Contact Us</div>
+
+        <a
+          href="mailto:ascenducr@gmail.com"
+          className="mt-1 text-2xl font-semibold underline decoration-1"
         >
-          Contact Us
-        </div>
-        <div className="mt-1 text-2xl font-semibold underline">
-          <a href="mailto:ascenducr@gmail.com">ascenducr@gmail.com</a>
-        </div>
+          ascenducr@gmail.com
+        </a>
+
         <div className="mt-4 flex items-center gap-8">
-          <Link href="https://linktr.ee/AscendUCR">
+          <Link
+            href="https://linktr.ee/AscendUCR"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <SiLinktree size={55} />
           </Link>
 
-          <Link href="https://www.linkedin.com/company/ascenducr-25">
+          <Link
+            href="https://www.linkedin.com/company/ascenducr-25"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FiLinkedin size={60} />
           </Link>
 
-          <Link href="https://www.instagram.com/ascend_ucr/">
+          <Link
+            href="https://www.instagram.com/ascend_ucr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagram size={60} />
           </Link>
         </div>
