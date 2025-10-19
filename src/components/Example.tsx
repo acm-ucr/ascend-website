fa47ef29777b2f07b325155a8f0b6de5dca30897
Ethan Chai <ethanc0301@gmail.com>
2025-10-19 00:46:52 -0700

    Reformatted example component

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,6 +1,6 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="w-2/3 flex-col items-center gap-8 rounded-lg bg-red-200 p-10 text-center text-xl">
+    <div className="flex w-2/3 flex-col items-center gap-8 rounded-lg bg-red-200 p-10 text-center text-xl">
       <div className="w-1/2 bg-blue-200 p-5">{props.text1}</div>
       <div className="flex w-1/2 flex-row gap-4">
         <div className="w-full rounded-full bg-blue-300 p-5">{props.text2}</div>
