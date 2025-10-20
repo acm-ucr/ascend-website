e33657fab40a16433195c4645cb02d4ced2ca09b
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-10-19 18:12:15 -0700

    ran prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@ export default function Header({ title = "About Us" }) {
 
   return (
     <div className="flex items-center justify-center">
- 
-      <div className="flex flex-col gap-2 mb-5">
+      <div className="mb-5 flex flex-col gap-2">
         <div className="flex gap-2">
           <span className={dot}></span>
           <span className={dot}></span>
@@ -18,11 +17,11 @@ export default function Header({ title = "About Us" }) {
         </div>
       </div>
 
+      <h2 className="mx-9 font-[Playfair_Display] text-2xl font-bold text-[#1a1a3b]">
+        {title}
+      </h2>
 
-      <h2 className="text-2xl font-[Playfair_Display] font-bold text-[#1a1a3b] mx-9">{title}</h2>
-
-
-      <div className="flex flex-col gap-2 items-end mt-5">
+      <div className="mt-5 flex flex-col items-end gap-2">
         <div className="flex gap-2">
           <span className={dot}></span>
         </div>
