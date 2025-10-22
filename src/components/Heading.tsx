0ed912b7cc9d40cf12dbb9b65ffd010a198a493b
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-10-21 19:19:03 -0700

    adjusted spacing

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,32 +3,32 @@ export default function Heading({ title = "About Us" }) {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="mb-5 flex flex-col gap-2">
-        <div className="flex gap-2">
+      <div className="mb-8 flex flex-col gap-3">
+        <div className="flex gap-3">
           <span className={dot}/>
           <span className={dot}/>
           <span className={dot}/>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-3">
           <span className={dot}/>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-3">
           <span className={dot}/>
         </div>
       </div>
 
-      <h2 className="mx-9 font-playfair text-6xl font-bold text-ascend-dark-blue">
+      <h2 className="mx-12 font-playfair text-6xl font-bold text-ascend-dark-blue">
         {title}
       </h2>
 
-      <div className="mt-5 flex flex-col items-end gap-2">
-        <div className="flex gap-2">
+      <div className="mt-8 flex flex-col items-end gap-3">
+        <div className="flex gap-3">
           <span className={dot}/>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-3">
           <span className={dot}/>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-3">
           <span className={dot}/>
           <span className={dot}/>
           <span className={dot}/>
