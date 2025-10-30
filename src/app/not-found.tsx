2a2f19e826cece5a989073a32d3bada6d931c0ea
vivimorecs <vmore003@ucr.edu>
2025-10-29 18:11:46 -0700

    Fixed link to homepage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,16 @@
-"use client";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const NotFound = () => {
-  const router = useRouter();
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="font-playfair text-ascend-red-orange text-8xl">404</h1>
       <p className="font-playfair mt-3 text-4xl font-bold">Page Not Found</p>
-      <button
-        onClick={() => router.push("/")}
+      <Link
+        href="/"
         className="bg-ascend-red-orange mt-5 rounded-full px-8 py-2 text-xl text-white transition hover:bg-red-700"
       >
         Back to Home
-      </button>
+      </Link>
     </div>
   );
 };
