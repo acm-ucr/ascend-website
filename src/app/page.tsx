9e2d46a605f800550ebd72eac8e6ddc5d7b78abe
Ampherost <Ampherost235@gmail.com>
2025-10-30 10:30:05 -0700

    landing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import NavBar from "@/components/NavBar";
+import Example from "@/components/Example";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* Navbar */}
-      <NavBar />
-
-      {/* Main content */}
-      <main className="flex flex-1 items-center justify-center">
-        <h1 className="text-3xl font-semibold text-gray-800">
-          Welcome to Ascend UC Riverside
-        </h1>
-      </main>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Example text1="adjust" text2="text2" text3="these" />
     </div>
   );
 };
