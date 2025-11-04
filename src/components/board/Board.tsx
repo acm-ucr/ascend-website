46cb330498ff75527c558500fd7f21850a0193a7
mialsong <mialsong@icloud.com>
2025-11-03 16:32:04 -0800

    added suggested changes to Board component

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -17,20 +17,20 @@ const Board = ({
   linkedin,
 }: BoardProps) => {
   return (
-    <div className="font-nunito relative">
+    <div className="font-nunito relative flex flex-col items-center">
       <Image
         src={profilePic}
         alt="profile Pic"
         className="relative max-h-115 max-w-115 rounded-3xl object-cover"
       />
-      <div className="grid h-25 w-80 -translate-y-40 auto-rows-min content-center bg-white/80 pl-5">
+      <div className="h-25 w-80 -translate-x-17.5 -translate-y-40 content-center bg-white/80 pl-5">
         <p className="text-2xl font-bold"> {name} </p>
         <p className="text-l"> {position} </p>
       </div>
       <div className="-translate-y-20">
-        <p className="pb-3 text-center text-xl">{majorYear}</p>
+        <p className="pb-3 text-xl">{majorYear}</p>
         <Link href={linkedin} target="_blank">
-          <p className="text-l bg-ascend-dark-blue w-fit justify-self-center rounded-full px-6 py-1 text-white underline decoration-0 underline-offset-2">
+          <p className="text-l bg-ascend-dark-blue justify-self-center rounded-full px-6 py-1 text-white underline underline-offset-2">
             {"LinkedIn"}
           </p>
         </Link>
