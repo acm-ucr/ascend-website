da2a2b000ad92506a3dae9546540e8091f46e6bd
kshuang04 <shuan284@ucr.edu>
2025-11-08 18:13:41 -0800

    kshuang04/EventCard

---
+++ src/components/events/EventCard.tsx
@@ -0,0 +1,32 @@
+interface componentProps {
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  description: string;
+}
+
+const EventCard = ({
+  title,
+  date,
+  time,
+  location,
+  description,
+}: componentProps) => {
+  return (
+    <div className="flex h-3/8 w-3/8 flex-col p-8">
+      <div className="font-playfair text-ascend-red-orange border-t-2 border-b-2 border-black pt-4 pb-3 text-center text-5xl font-bold">
+        {title}
+      </div>
+      <div className="py-3 text-left text-3xl font-semibold">
+        <div>
+          {date} | {time}
+        </div>
+        <div>{location}</div>
+      </div>
+      <div className="text-left text-2xl font-thin indent-8">{description}</div>
+    </div>
+  );
+};
+
+export default EventCard;
