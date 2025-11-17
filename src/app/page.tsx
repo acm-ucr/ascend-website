d67f3d7594d650db86a2570f4ece6101674c61e3
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-11-17 14:34:31 -0800

    removed carousel call from main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Carousel from "@/components/Carousel";
-import partners from "@/data/partners";
+
 
 const Home = () => {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
       Home
-      <Carousel data={partners} />
     </div>
   );
 };
