a17257b77cd7bc52e43f805dd463bc96e701003c
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-11-24 18:26:03 -0800

    removed mentors call in join us page

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,10 +1,8 @@
 import JoinUs from "@/components/join/JoinUs";
-import MeetOurMentors from "@/components/join/MeetOurMentors";
 const Join = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center">
       <JoinUs />
-      <MeetOurMentors />
     </div>
   );
 };
