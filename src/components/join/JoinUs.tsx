a9c57daed45aa455fc72ff41de8ff2b235b08430
EveningSt3r <daawesomeboi@gmail.com>
2025-11-29 03:38:44 -0800

    Prettier fix

--- src/components/join/JoinUs.tsx
+++ src/components/join/JoinUs.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 const JoinUs = () => {
   return (
     <div className="min-h-screen">
-        <div className="flex flex-col items-center justify-center pt-20 not-visited:flex-col md:flex">
-              <Heading title="Join Us" />
-              <p className="text-ascend-dark-blue mt-15 w-2/3 text-center text-2xl">
-                Ascend is a national non-profit organization that is advancing
-                Pan-Asian professionals across all industries. We welcome
-                students of every major who are passionate about leadership,
-                growth, and community.
-              </p>
+      <div className="flex flex-col items-center justify-center pt-20 not-visited:flex-col md:flex">
+        <Heading title="Join Us" />
+        <p className="text-ascend-dark-blue mt-15 w-2/3 text-center text-2xl">
+          Ascend is a national non-profit organization that is advancing
+          Pan-Asian professionals across all industries. We welcome students of
+          every major who are passionate about leadership, growth, and
+          community.
+        </p>
 
-              <div className="relative my-15 h-96 w-2/3">
-                <Image
-                  src="/home/ascend.webp"
-                  alt="Logo"
-                  fill
-                  className="w-full object-cover md:h-full"
-                  priority
-                />
-            </div>
-          </div>
+        <div className="relative my-15 h-96 w-2/3">
+          <Image
+            src="/home/ascend.webp"
+            alt="Logo"
+            fill
+            className="w-full object-cover md:h-full"
+            priority
+          />
         </div>
+      </div>
+    </div>
   );
 };
 
